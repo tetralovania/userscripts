4d3effac80658efafc76190c50e03c8e70906f8a
tetralovania <86831655+tetralovania@users.noreply.github.com>
2023-06-11 12:39:52 +0200

    Update moodle.user.js
    - added lsf, boss, räume to header
    - moved kurssuche, support, faq into more section in header

--- moodle.user.js
+++ moodle.user.js
@@ -36,7 +36,10 @@ const addCourseNameToCalendar = async () => {
   }
   const savedCourses = await courseNamesToMap()
   for(let index = 0; index < 10; index++){
-    document.evaluate(calMatch, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null).snapshotItem(index).insertAdjacentHTML("beforeBegin",savedCourses.get(calEvents[index]) + " - ");
+    var courseText = savedCourses.get(calEvents[index]);
+    var courseLink = calEvents[index];
+    var finalCourseName = '<a href="course/view.php?id=' + calEvents[index] + '">' + courseText + '</a>';
+    document.evaluate(calMatch, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null).snapshotItem(index).insertAdjacentHTML("beforeBegin",finalCourseName + " - ");
   }
 }
 
@@ -181,6 +184,19 @@ if(window.location.href.match("https://moodle.tu-dortmund.de/login/index.php")){
   window.location.href="https://sso.itmc.tu-dortmund.de/openam/XUI/?goto=https%3A%2F%2Fmoodle.tu-dortmund.de%2Flogin#login/"
 }
 
+
+// replace link to main page
+for (i = 0; i < document.links.length; i++) {
+  var link = document.links[i];
+  var match = link.href.match("https://www.tu-dortmund.de/");
+
+  if (match != null) {
+      link.href = "https://moodle.tu-dortmund.de/my/";
+      break;
+  }
+}
+
+
 // change colors
 addGlobalStyle('body {background-color: var(--backgroundColor) !important;}'); //background
 addGlobalStyle('#page.drawers .main-inner{background-color: var(--darkAccent) !important; padding: 4rem 0.3rem 0 0.3rem; margin: 0;}');
@@ -229,10 +245,148 @@ addGlobalStyle('#moremenu-6462516b87efe-navbar-nav > li:nth-child(5){innerHTML="
 // add Course to Calendar Elements on main page
 addCourseNameToCalendar();
 
+//add Elements to header bar
+// add LSF
+  // Create a new list item
+  var newItem = document.createElement("li");
+  newItem.setAttribute("data-key", "");
+  newItem.setAttribute("class", "nav-item");
+  newItem.setAttribute("role", "none");
+  newItem.setAttribute("data-forceintomoremenu", "false");
+
+  // Create a new link within the list item
+  var newLink = document.createElement("a");
+  newLink.setAttribute("role", "menuitem");
+  newLink.setAttribute("class", "nav-link");
+  newLink.setAttribute("href", "https://www.lsf.tu-dortmund.de/qisserver/rds;jsessionid=7A73A100D7E7C444C9B09434CC2A1D3A.lsf4?state=user&type=0&category=menu.browse&breadCrumbSource=&startpage=portal.vm");
+  newLink.setAttribute("tabindex", "-1");
+  newLink.textContent = "LSF";
+
+  // Append the link to the list item
+  newItem.appendChild(newLink);
+
+  // Find the existing navigation list
+  var navList = document.querySelector(".primary-navigation .nav");
+
+  // Append the new list item to the navigation list
+  navList.appendChild(newItem);
+
+//add BOSS
+// Create a new list item
+  var newItem = document.createElement("li");
+  newItem.setAttribute("data-key", "");
+  newItem.setAttribute("class", "nav-item");
+  newItem.setAttribute("role", "none");
+  newItem.setAttribute("data-forceintomoremenu", "false");
+
+  // Create a new link within the list item
+  var newLink = document.createElement("a");
+  newLink.setAttribute("role", "menuitem");
+  newLink.setAttribute("class", "nav-link");
+  newLink.setAttribute("href", "https://www.boss.tu-dortmund.de/qisserver/rds?state=user&type=0&category=menu.browse&breadCrumbSource=&startpage=portal.vm&chco=y");
+  newLink.setAttribute("tabindex", "-1");
+  newLink.textContent = "BOSS";
+
+  // Append the link to the list item
+  newItem.appendChild(newLink);
+
+  // Find the existing navigation list
+  var navList = document.querySelector(".primary-navigation .nav");
+
+  // Append the new list item to the navigation list
+  navList.appendChild(newItem);
 
 
+//add Raumbuchung
+// Create a new list item
+  var newItem = document.createElement("li");
+  newItem.setAttribute("data-key", "");
+  newItem.setAttribute("class", "nav-item");
+  newItem.setAttribute("role", "none");
+  newItem.setAttribute("data-forceintomoremenu", "false");
+
+  // Create a new link within the list item
+  var newLink = document.createElement("a");
+  newLink.setAttribute("role", "menuitem");
+  newLink.setAttribute("class", "nav-link");
+  newLink.setAttribute("href", "https://raumadm.cs.tu-dortmund.de/cont/de/lernraum/ssl/kalender.sh?id=364d68b407d4a8ad62812a0071b386fd175d01a0490017f3");
+  newLink.setAttribute("tabindex", "-1");
+  newLink.textContent = "Räume";
+
+  // Append the link to the list item
+  newItem.appendChild(newLink);
+
+  // Find the existing navigation list
+  var navList = document.querySelector(".primary-navigation .nav");
+
+  // Append the new list item to the navigation list
+  navList.appendChild(newItem);
+
+//force useless stuff into more menu
+ document.querySelector('.primary-navigation').onload = forceintomoremenu();
 
 
+function forceintomoremenu() {
+  // Find the primary-navigation element
+  var primaryNav = document.querySelector('.primary-navigation');
+
+  // Create a new list element for the More menu
+  var moreMenu = document.createElement('li');
+  moreMenu.setAttribute('role', 'none');
+  moreMenu.setAttribute('class', 'nav-item dropdown dropdownmoremenu');
+  moreMenu.setAttribute('data-region', 'morebutton');
+
+  // Create a new link for the More menu
+  var moreLink = document.createElement('a');
+  moreLink.setAttribute('class', 'dropdown-toggle nav-link');
+  moreLink.setAttribute('href', '#');
+  moreLink.setAttribute('id', 'moremenu-dropdown-64801d6f87355');
+  moreLink.setAttribute('role', 'menuitem');
+  moreLink.setAttribute('data-toggle', 'dropdown');
+  moreLink.setAttribute('aria-haspopup', 'true');
+  moreLink.setAttribute('aria-expanded', 'false');
+  moreLink.setAttribute('tabindex', '-1');
+  moreLink.textContent = 'Mehr';
+
+  // Create a new list for the dropdown menu
+  var dropdownMenu = document.createElement('ul');
+  dropdownMenu.setAttribute('class', 'dropdown-menu dropdown-menu-left');
+  dropdownMenu.setAttribute('data-region', 'moredropdown');
+  dropdownMenu.setAttribute('aria-labelledby', 'moremenu-dropdown-64801d6f87355');
+  dropdownMenu.setAttribute('role', 'menu');
+
+  // Find the "Support" and "FAQ" list items
+  var supportItem = primaryNav.childNodes[1].childNodes[1].childNodes[7];
+  var faqItem = primaryNav.childNodes[1].childNodes[1].childNodes[9];
+
+  // Add dropdown-item class to the items
+  supportItem.setAttribute('class', 'dropdown-item');
+  faqItem.setAttribute('class', 'dropdown-item');
+
+  // Append the "Support" and "FAQ" list items to the dropdown menu
+  dropdownMenu.appendChild(supportItem);
+  dropdownMenu.appendChild(faqItem);
+
+  // Append the dropdown menu and link to the More menu
+  moreMenu.appendChild(moreLink);
+  moreMenu.appendChild(dropdownMenu);
+
+  // Append the More menu to the primary-navigation element
+  primaryNav.querySelector('ul.nav').appendChild(moreMenu);
+
+  dropdownMenu.onload = modifyMoreMenu(dropdownMenu);
+}
+
+  function modifyMoreMenu(dropdownMenu){
+  // Modify the CSS for each dropdown item
+  var dropdownItems = dropdownMenu.querySelectorAll('.dropdown-item');
+  dropdownItems.forEach(function(item) {
+    item.style.height = "30.5px !important";
+    item.style.position = "relative";
+    item.style.top = "0 !important";
+    item.style.border = "0 !important";
+    });
+  }
 
 
 
